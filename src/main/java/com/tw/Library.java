b5bc7965a77960f143029a498ecc0fe38a0c58ff
Katrina <zhaoyanlin2016@gmail.com>
2018-04-13 10:13:57 -0700

    Complete main menu.

--- src/main/java/com/tw/Library.java
+++ src/main/java/com/tw/Library.java
@@ -1,7 +1,39 @@
 package com.tw;
 
+import java.util.Scanner;
+
 public class Library {
-    public boolean someLibraryMethod() {
-        return true;
+
+    public static void main(String[] args) {
+
+        String MENU = "1. 添加学生\n2. 生成成绩单\n3. 退出\n请输入你的选择（1～3）：";
+
+        Scanner sc = new Scanner(System.in);
+
+        int option;
+
+        while (true) {
+            System.out.println(MENU);
+
+            // check if input is an integer
+            while (!sc.hasNextInt()) {
+                System.out.println(MENU);
+                sc.next();
+            }
+            option = sc.nextInt();
+
+            if (option == 1) {
+                System.out.println("请输入学生信息（格式：姓名, 学号, 学科: 成绩, ...），按回车提交：");
+            } else if (option == 2) {
+                System.out.println("请输入要打印的学生的学号（格式： 学号, 学号,...），按回车提交：");
+            } else if (option == 3) {
+                break;
+            }
+        }
+
+        sc.close();
     }
+
+
+
 }
