af508ae8836f5fc54f2165d335eba402dbebcbf2
Katrina <zhaoyanlin2016@gmail.com>
2018-04-16 11:57:10 -0700

    Reconstruct the printRecords method.

--- src/test/java/com/tw/LibraryTest.java
+++ src/test/java/com/tw/LibraryTest.java
@@ -13,7 +13,7 @@ public class LibraryTest {
     @Test
     public void testSomeLibraryMethod() {
         Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+        //assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 
     @Test
---
+++ src/test/java/com/tw/ReportSystemTest.java
@@ -0,0 +1,76 @@
+package com.tw;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class ReportSystemTest {
+
+    ReportSystem reportSystem;
+
+    @Before
+    public void setUp() throws Exception {
+        reportSystem = new ReportSystem();
+    }
+
+    @Test
+    public void getSingleRecord() {
+        reportSystem.addRecord("Adam, 001, 数学: 97, 语文: 80, 英语: 85, 编程: 99");
+        assertTrue(reportSystem.getSingleRecord("001").equals(new StudentRecord("Adam", "001")));
+    }
+
+    @Test
+    public void getRecords() {
+
+        assertTrue(reportSystem.getRecords() instanceof HashMap);
+
+    }
+
+    @Test
+    public void addRecord() {
+
+        StudentRecord lucy = new StudentRecord("Lucy", "002");
+
+        reportSystem.addRecord("Lucy, 002, 数学: 90, 语文: 98, 英语: 90, 编程: 100");
+        reportSystem.addRecord("Alex, 003, 数学: 97, 语文: 80, 英语: 85, 编程: 99");
+
+        assertEquals(reportSystem.getRecords().get("002"), lucy);   // Student records with the same id are considered as the same
+        assertFalse(reportSystem.getRecords().get("003").equals(lucy));
+
+    }
+
+    @Test
+    public void validateIDs() {
+        ReportSystem reportSystem = new ReportSystem();
+
+        String[] validIDs = {"001", "111", "1024", "999"};
+        String[] invalidIDs1 = {"a"};
+        String[] invalidIDs2 = {"1.01"};
+        String[] invalidIDs3 = {"张三"};
+        String[] invalidIDs4 = {"Adam"};
+
+        assertTrue(reportSystem.validateIDs(validIDs));
+        assertFalse(reportSystem.validateIDs(invalidIDs1));
+        assertFalse(reportSystem.validateIDs(invalidIDs2));
+        assertFalse(reportSystem.validateIDs(invalidIDs3));
+        assertFalse(reportSystem.validateIDs(invalidIDs4));
+    }
+
+    @Test
+    public void getMedian() {
+        ReportSystem reportSystem = new ReportSystem();
+
+        List<Double> list = new ArrayList<>();
+        list.add(98.0);
+        list.add(80.0);
+        list.add(100.0);
+        list.add(95.0);
+
+        assertTrue(reportSystem.getMedian(list) == 96.5);
+    }
+}
